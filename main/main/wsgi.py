48feada62af1829509b18fb680aa541883b2cfe1
emekadefirst <victorchibuogwu33@gmail.com>
2024-08-24 07:53:08 +0100

    koyeb

--- main/main/wsgi.py
+++ main/main/wsgi.py
@@ -1,18 +1,8 @@
-"""
-WSGI config for main project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the default settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
+# Get the WSGI application
 application = get_wsgi_application()
-
-app = application
